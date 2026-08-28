2139a19a62fc2cd7e638b15686b402142daa6cc1
Md. Nihal Uddin Bijoy <mdnihaluddinbijoy@gmail.com>
2026-08-28 07:22:27 +0600

    Add Vercel serverless entry point

---
+++ api/index.ts
@@ -0,0 +1,12 @@
+import app from "../src/app";
+import { connectDB } from "../src/config/db";
+
+let isConnected = false;
+
+export default async function handler(req: any, res: any) {
+  if (!isConnected) {
+    await connectDB();
+    isConnected = true;
+  }
+  return app(req, res);
+}
