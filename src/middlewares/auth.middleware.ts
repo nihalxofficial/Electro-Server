413cccd4c6cc22bbd0d5ef16f0c63e2eccb9b2bd
Md. Nihal Uddin Bijoy <mdnihaluddinbijoy@gmail.com>
2026-09-17 08:40:10 +0600

    Auth middleware added

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,42 @@
+import { Request, Response, NextFunction } from "express";
+import { ApiError } from "../utils/apiError";
+import { ENV } from "../config/env";
+
+const { createRemoteJWKSet, jwtVerify } = require("jose-cjs");
+
+const JWKS = createRemoteJWKSet(new URL(`${ENV.AUTH_SERVER_URL}/api/auth/jwks`));
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: {
+        id: string;
+        email?: string;
+        role?: string;
+      };
+    }
+  }
+}
+
+export async function requireAuth(req: Request, res: Response, next: NextFunction) {
+  const token = req.headers.authorization?.split(" ")[1] || (req.query.token as string);
+
+  if (!token) {
+    return next(new ApiError(401, "No token provided"));
+  }
+
+  try {
+    const { payload } = await jwtVerify(token, JWKS, {
+      issuer: ENV.AUTH_SERVER_URL,
+    });
+
+    req.user = {
+      id: payload.sub as string,
+      email: payload.email as string | undefined,
+      role: payload.role as string | undefined,
+    };
+    next();
+  } catch (err) {
+    next(new ApiError(401, "Invalid or expired token"));
+  }
+}
